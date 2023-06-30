419bc0589caae267e8533a105a0b042ff9c21515
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-30 18:54:37 +0530

    designed the result page

--- screens/result.js
+++ screens/result.js
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet,Image, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Result = () => {
   return (
     <View>
-      
+      <View>
+      <Text>Result</Text>
+      </View>
+      <View style={styles.bannerContainer}>
+      <Image source={require('../assets/quiz.jpg')}
+      style={styles.banner}
+      resizeMode='contain'
+      />
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Home</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default Result
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  banner:{
+    width:300,
+    height:300,
+  },
+  bannerContainer:{
+    justifyContent:'center',
+    alignItems:'center'
+
+  }
+})
